fc419d2cf0d29d3ed86139e546913ed6ea1d093e
simon <simonmontenegro55@gmail.com>
2024-09-06 13:35:10 -0300

    Se agrega carga de datos csv

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using TP_01.models;
+
+class Ejemplo {
+    static void Main()
+    {
+        string cadeteFilePath = "./DB/cadete.csv";
+        string cadeteriaFilePath = "./DB/cadete.csv";
+        string separadorCsv = ";";
+
+        Cadeteria nuevaCadeteria = new();
+
+        string? line;
+
+        using StreamReader srCadeteria = File.OpenText(cadeteriaFilePath);
+        while ((line = srCadeteria.ReadLine()) != null)
+        {
+            string[] datosCadeteria = line.Trim().Split(separadorCsv);
+            string nombre = datosCadeteria[0];
+            string telefono = datosCadeteria[1];
+            nuevaCadeteria.Nombre = nombre;
+            nuevaCadeteria.Telefono = telefono;
+        }
+
+        using StreamReader srCadete = File.OpenText(cadeteFilePath);
+        while ((line = srCadete.ReadLine()) != null)
+        {
+            string[] datosCadeteria = line.Trim().Split(separadorCsv);
+            _ = uint.TryParse(datosCadeteria[0], out uint id);
+            string nombre = datosCadeteria[1];
+            string direccion = datosCadeteria[2];
+            string telefono = datosCadeteria[3];
+            Cadete nuevoCadete = new Cadete(id,nombre,direccion,telefono);
+            nuevaCadeteria.AgregarCadete(nuevoCadete);
+        }
+    }
+}
--- models/Cadeteria.cs
+++ models/Cadeteria.cs
@@ -1,8 +1,8 @@
 namespace TP_01.models;
 class Cadeteria
 {
-    public string Nombre { get; set; }
-    public string Telefono { get; set; }
+    public string? Nombre { get; set; }
+    public string? Telefono { get; set; }
     public List<Cadete> ListaCadetes { get; private set; }
 
     public Cadeteria(string nombre, string telefono, List<Cadete> listaCadetes)
@@ -11,7 +11,15 @@ class Cadeteria
         Telefono = telefono;
         ListaCadetes = listaCadetes ?? new List<Cadete>();
     }
+    public Cadeteria() {
+        ListaCadetes =  new List<Cadete>();
+    }
     public Cadeteria(string nombre, string telefono)
     : this(nombre, telefono, new List<Cadete>()) { }
+
+    public void AgregarCadete(Cadete nuevoCadete) 
+    {
+        if (nuevoCadete != null) ListaCadetes.Add(nuevoCadete);;
+    }
 }
 
